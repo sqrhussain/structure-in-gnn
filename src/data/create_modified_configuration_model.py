572bd1c9355c01be7f661f4a08437c8e20f8b5fd
Hussain Hussain <hhussain@know-center.at>
2021-03-10 21:28:27 +0100

    fixed modcm code

--- src/data/create_modified_configuration_model.py
+++ src/data/create_modified_configuration_model.py
@@ -1,5 +1,7 @@
 import networkx as nx
 from networkx.utils import powerlaw_sequence
+import os
+from src.data.create_stochastic_block_model import load_labels
 
 def generate_conf_model(G,seed=0):
     din=[x[1] for x in G.in_degree()]
@@ -14,7 +16,9 @@ def generate_conf_model(G,seed=0):
 
 def generate_modified_conf_model(G, seed=0):
     node_labels_dict = nx.get_node_attributes(G,'label')
-    unique_node_labels = set(node_labels.values())
+    # print(type(node_labels_dict))
+    # print(list(node_labels_dict.values()))
+    unique_node_labels = set(node_labels_dict.values())
     same_label_subgraphs = {}
     for node_label in unique_node_labels:
         same_label_subgraphs[node_label] = nx.DiGraph()
@@ -30,10 +34,13 @@ def generate_modified_conf_model(G, seed=0):
     return G
 
 
-def generate_multiple_modified_conf_models(graph_path, output_prefix,output_suffix = '.cites',inits=10):
+def generate_multiple_modified_conf_models(graph_path, content_path, output_prefix,output_suffix = '.cites',inits=10):
     fin = graph_path
     G=nx.read_edgelist(fin,create_using=nx.DiGraph())
+    node_labels_dict,_ = load_labels(content_path)
+    nx.set_node_attributes(G,node_labels_dict, name='label')
     for i in range(inits):
+        print(f'{graph_path} ... {output_prefix}_{i}{output_suffix}')
         GNoisy = generate_modified_conf_model(G,seed=i)
         fout = f'{output_prefix}_{i}{output_suffix}'
         nx.write_edgelist(GNoisy,fout)
@@ -41,6 +48,14 @@ def generate_multiple_modified_conf_models(graph_path, output_prefix,output_suff
 
 
 if __name__ == '__main__':
+    datasets = 'cora citeseer twitter chameleon squirrel webkb actor pubmed cora_full'.split()
+    for dataset in datasets:
+
+        if not os.path.exists(f'data/graphs/modcm/{dataset}/'):
+            os.mkdir(f'data/graphs/modcm/{dataset}/')
+        generate_multiple_modified_conf_models(f'data/graphs/processed/{dataset}/{dataset}.cites',
+                                f'data/graphs/processed/{dataset}/{dataset}.content',
+                                f'data/graphs/modcm/{dataset}/{dataset}_modcm')
     # generate_multiple_modified_conf_models('data/graphs/processed/cora/cora.cites','data/graphs/confmodel/cora/cora_confmodel')
     # generate_multiple_modified_conf_models('data/graphs/processed/citeseer/citeseer.cites','data/graphs/confmodel/citeseer/citeseer_confmodel')
     # generate_multiple_modified_conf_models('data/graphs/processed/pubmed/pubmed.cites','data/graphs/confmodel/pubmed/pubmed_confmodel')
@@ -59,5 +74,5 @@ if __name__ == '__main__':
     #                     'data/graphs/confmodel/ms_academic_cs/ms_academic_cs_confmodel')
     # generate_multiple_modified_conf_models('data/graphs/processed/ms_academic_phy/ms_academic_phy.cites',
     #                     'data/graphs/confmodel/ms_academic_phy/ms_academic_phy_confmodel')
-    generate_multiple_modified_conf_models('data/graphs/processed/wiki_cs/wiki_cs.cites',
-                        'data/graphs/confmodel/wiki_cs/wiki_cs_confmodel')
+    # generate_multiple_modified_conf_models('data/graphs/processed/wiki_cs/wiki_cs.cites',
+    #                     'data/graphs/confmodel/wiki_cs/wiki_cs_confmodel')
